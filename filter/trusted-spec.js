660e402949c966c817c8ea81a6b6ba461ce5d33e
blaircalderwood@live.co.uk <blaircalderwood@live.co.uk>
2016-11-03 09:36:48 +0000

    Added trusted URL filter

---
+++ filter/trusted-spec.js
@@ -0,0 +1,13 @@
+describe('trusted', function() {
+
+    beforeEach(module('cv'));
+
+    it('should ...', inject(function($filter) {
+
+        var filter = $filter('trusted');
+
+        expect(filter('input')).toEqual('output');
+
+    }));
+
+});
---
+++ filter/trusted.js
@@ -0,0 +1,5 @@
+angular.module('cv').filter('trusted', ['$sce', function ($sce) {
+    return function (url) {
+        return $sce.trustAsResourceUrl(url);
+    };
+}]);
